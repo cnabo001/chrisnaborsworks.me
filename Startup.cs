5a4833d8a5c74420680c94141f96a53a7e3f32af
cnabo001 <46269040+cnabo001@users.noreply.github.com>
2025-01-15 12:04:00 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -35,108 +35,103 @@ namespace ChrisNaborsWorks
 
 		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
 			
-			services.AddSingleton(_config);
-			services.AddMvc(config =>
+		services.AddSingleton(_config);
+		services.AddMvc(config =>
+		{
+			if (_hos.IsProduction())
 			{
-				if (_hos.IsProduction())
-				{
-					config.Filters.Add(new RequireHttpsAttribute());
-				}
-			})
-				.AddJsonOptions(op =>
-				{
-					op.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-				}
-				);
-
-			services.AddIdentity<ChrisNaborsWorksUser, IdentityRole>(config =>
-			{
-				config.User.RequireUniqueEmail = true;
-				config.Password.RequiredLength = 8;
-				//config.Password.RequireDigit = true;
+				config.Filters.Add(new RequireHttpsAttribute());
 			}
-			  ).AddEntityFrameworkStores<DataContext>();
-
-			services.ConfigureApplicationCookie (config =>
+		})
+			.AddJsonOptions(op =>
 			{
-				config.LoginPath = "/Auth/Login";
-				config.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents()
-				{
-					OnRedirectToLogin = async ctx =>
-					{
-						if (ctx.Request.Path.StartsWithSegments("/api") &&
-						ctx.Response.StatusCode == 200)
-						{
-							ctx.Response.StatusCode = 401;
-						}
-						else
-						{
-							ctx.Response.Redirect(ctx.RedirectUri);
-						}
-						await Task.Yield();
-					}
-				};
-			});
-			services.AddAuthentication();
-				//.AddFacebook(config =>
-				//{
-				//	config.AppId = _config["auth:facebook:appid"];
-				//	config.AppSecret = _config["auth:facebook:appsecret"];
-				//});
+				op.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			}
+			);
 
-			services.AddEntityFrameworkSqlServer();
-			services.AddScoped<IMailService, DebugMailService>();
-			services.AddTransient<CNWorksSeedData>();
-			services.AddDbContext<DataContext>();
-			services.AddScoped<ICNWRepository, CNWRepository>();
-			services.AddTransient<CryptoPriceService>();
-			services.AddLogging();
-		
+		services.AddIdentity<ChrisNaborsWorksUser, IdentityRole>(config =>
+		{
+			config.User.RequireUniqueEmail = true;
+			config.Password.RequiredLength = 8;
+			//config.Password.RequireDigit = true;
 		}
+		  ).AddEntityFrameworkStores<DataContext>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		services.ConfigureApplicationCookie (config =>
+		{
+			config.LoginPath = "/Auth/Login";
+			config.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents()
+			{
+				OnRedirectToLogin = async ctx =>
+				{
+					if (ctx.Request.Path.StartsWithSegments("/api") &&
+					ctx.Response.StatusCode == 200)
+					{
+						ctx.Response.StatusCode = 401;
+					}
+					else
+					{
+						ctx.Response.Redirect(ctx.RedirectUri);
+					}
+					await Task.Yield();
+				}
+			};
+		});
+		services.AddAuthentication();
+			//.AddFacebook(config =>
+			//{
+			//	config.AppId = _config["auth:facebook:appid"];
+			//	config.AppSecret = _config["auth:facebook:appsecret"];
+			//});
+
+		services.AddEntityFrameworkSqlServer();
+		services.AddScoped<IMailService, DebugMailService>();
+		services.AddTransient<CNWorksSeedData>();
+		services.AddDbContext<DataContext>();
+		services.AddScoped<ICNWRepository, CNWRepository>();
+		services.AddTransient<CryptoPriceService>();
+		services.AddLogging();
+		
+	}
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger
 			,CNWorksSeedData seeds, ILoggerFactory factory)
         {
-			Mapper.Initialize(config =>
-			{
-				config.CreateMap<LikesViewModel, Likes>().ReverseMap();
-				config.CreateMap<StoreViewModel, Store>().ReverseMap();
-			});
+	     Mapper.Initialize(config =>
+	     {
+		config.CreateMap<LikesViewModel, Likes>().ReverseMap();
+		config.CreateMap<StoreViewModel, Store>().ReverseMap();
+	     });
+		     
+	    if (env.IsDevelopment())
+	    {
+		app.UseBrowserLink();
+		app.UseDeveloperExceptionPage();
+	    }
+	    else
+	    {
+		app.UseExceptionHandler("/Home/Error");
+	    }
 
+	    factory.AddDebug();
 
+	    app.UseStaticFiles();
+	    app.UseAuthentication();
 
-			if (env.IsDevelopment())
+	    app.UseMvc(config =>
             {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-			factory.AddDebug();
-
-			app.UseStaticFiles();
-			app.UseAuthentication();
-
-			app.UseMvc(config =>
-            {
-				config.MapRoute(
-					name: "Default",
-					template: "{controller}/{action}/{id?}",
-					defaults: new { Controller = "Home", action = "Index" });
+		config.MapRoute(
+			name: "Default",
+			template: "{controller}/{action}/{id?}",
+			defaults: new { Controller = "Home", action = "Index" });
             });
-			seeds.ConfimSeedData().Wait();
+	    seeds.ConfimSeedData().Wait();
         }
 
     }
 
-	
 }
